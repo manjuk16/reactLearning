a12adb521e635ae184dc5eae81b15994116a11d5
manjuk16 <33445585+manjuk16@users.noreply.github.com>
2019-01-06 23:21:06 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       {name: "React", age:"29"},
       {name: "CSS", age: "27"}
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -25,12 +26,18 @@ class App extends Component {
   }
   
   nameChangedHandler = (event) => {
+
     this.setState({persons: [
         {name: "Manju", age: "28"},
         {name: event.target.value, age:"29"},
         {name: "CSS", age: "37"}
       ] 
     })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
@@ -42,23 +49,33 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Manju K')}
+          changed={this.nameChangedHandler}
+          >My Hobbies : Coding</Person>
+          <Person 
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}></Person>
+          <Person 
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}></Person>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really Working...!!!</p>
-        <button style={styleBtn} onClick={() => this.switchNameHandler("Manjunatha K")}>Switch Name</button>
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}
-        click={this.switchNameHandler.bind(this, 'Manju K')}
-        changed={this.nameChangedHandler}
-        >My Hobbies : Coding</Person>
-        <Person 
-        name={this.state.persons[1].name}
-         age={this.state.persons[1].age}></Person>
-        <Person 
-        name={this.state.persons[2].name}
-         age={this.state.persons[2].age}></Person>
+        <button style={styleBtn} onClick={() => this.togglePersonsHandler()}>Toggle Persons</button>
+        {persons}
       </div>
     );
   }
