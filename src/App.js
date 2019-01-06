b7042a8b0c360aaac4cfcb22988b7d96c234d25a
manjuk16 <33445585+manjuk16@users.noreply.github.com>
2019-01-06 22:40:37 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -34,11 +34,19 @@ class App extends Component {
   }
 
   render() {
+    const styleBtn = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really Working...!!!</p>
-        <button onClick={() => this.switchNameHandler("Manjunatha K")}>Switch Name</button>
+        <button style={styleBtn} onClick={() => this.switchNameHandler("Manjunatha K")}>Switch Name</button>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm a {props.name} and am {props.age} years old..!</p>
             <p>{props.children}</p>
             <input type="texts" placeholder="New Name" onChange={props.changed} value={props.name}/>
