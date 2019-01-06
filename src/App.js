a7acdaad85d2c981a29a498f05e8d2d974a0aadd
manjuk16 <33445585+manjuk16@users.noreply.github.com>
2019-01-06 20:51:16 +0530

    Initial SetUp with basics of React

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    persons: [
+      {name: "Manju", age: "28"},
+      {name: "React", age:"29"},
+      {name: "CSS", age: "27"}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    //console.log("Button Clicked");
+    // this.state.persons[0].name = "Natha K"
+    this.setState({persons: [
+        {name: newName, age: "28"},
+        {name: "React", age:"29"},
+        {name: "CSS", age: "27"}
+      ] 
+    })
+  }
+  
+  nameChangedHandler = (event) => {
+    this.setState({persons: [
+        {name: "Manju", age: "28"},
+        {name: event.target.value, age:"29"},
+        {name: "CSS", age: "37"}
+      ] 
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <p>This is really Working...!!!</p>
+        <button onClick={() => this.switchNameHandler("Manjunatha K")}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}
+        click={this.switchNameHandler.bind(this, 'Manju K')}
+        changed={this.nameChangedHandler}
+        >My Hobbies : Coding</Person>
+        <Person 
+        name={this.state.persons[1].name}
+         age={this.state.persons[1].age}></Person>
+        <Person 
+        name={this.state.persons[2].name}
+         age={this.state.persons[2].age}></Person>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div>
+            <p onClick={props.click}>I'm a {props.name} and am {props.age} years old..!</p>
+            <p>{props.children}</p>
+            <input type="texts" placeholder="New Name" onChange={props.changed} value={props.name}/>
+        </div>
+    )
+}
+
+export default person;
